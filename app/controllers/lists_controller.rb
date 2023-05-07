b6cc811f435b0cfbff4b43f298f7209a50993896
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-05-07 03:54:22 +0000

    [3]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,16 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
   end
 
   def index
+    @lists = List.all
   end
 
   def show
@@ -10,4 +18,11 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+  
 end
